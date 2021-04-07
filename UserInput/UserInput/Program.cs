efc4f85666b8990370de3d3c2b25205c5ed7b878
RocqJones <jonesmbindyo@gmail.com>
2021-04-07 11:47:12 +0300

    gettig user input

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name? ");
+            string fName = Console.ReadLine();
+
+            Console.WriteLine("Enter Second Name? ");
+            string sName = Console.ReadLine();
+
+            Console.WriteLine("Your age?");
+            string uAge = Console.ReadLine();
+
+            Console.WriteLine("Your full name is: " + fName + " " + sName + " and you are " + uAge + " years old");
+        }
+    }
+}
