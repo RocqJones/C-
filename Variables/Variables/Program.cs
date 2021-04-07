5dae10e6c3224fdd17cbc5b84512c4f6cee8f92f
RocqJones <jonesmbindyo@gmail.com>
2021-04-07 11:16:40 +0300

    variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John Doe";
+            int age = 23;
+            bool isMale = true;
+            char grade = 'A';
+            double salary = 200400.44D;
+            int months = 12;
+
+            double gross = salary * months;
+
+            Console.WriteLine(name);
+            Console.WriteLine("Age: " + age + " with grade " + grade);
+            Console.WriteLine(name + " earnes Ksh." + gross + " anually.");
+            Console.WriteLine(name + " is a male: " + isMale);
+        }
+    }
+}
