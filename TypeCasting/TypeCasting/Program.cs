ca1caf67e0d7e387747b72e10db8b8ea08fff626
RocqJones <jonesmbindyo@gmail.com>
2021-04-07 11:21:36 +0300

    Type casting

---
+++ TypeCasting/TypeCasting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TypeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble = 74.87;
+            int myInt = (int) myDouble;
+
+            Console.WriteLine(myDouble);
+            Console.WriteLine(myInt);
+        }
+    }
+}
