a157dc6a712f53ed7016da75c59d15212a5fc1a8
RocqJones <jonesmbindyo@gmail.com>
2021-04-07 13:11:43 +0300

    math functions

---
+++ Math/Math/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathXY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum: " + Math.Max(23,66));
+            Console.WriteLine("Minimum: " + Math.Min(23,66));
+            Console.WriteLine("Squire Root: " + Math.Sqrt(64));
+            Console.WriteLine("Absolute Value of -4.7: " + Math.Abs(-4.7));
+            Console.WriteLine("Round off 99.99: " + Math.Round(99.99));
+        }
+    }
+}
