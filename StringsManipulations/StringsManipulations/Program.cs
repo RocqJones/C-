d713c392b1619dae95d41abb90f7b3576dc1f05d
RocqJones <jonesmbindyo@gmail.com>
2021-04-07 13:21:13 +0300

    string manipulation

---
+++ StringsManipulations/StringsManipulations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringsManipulations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "GFRRCTVBNFDNCGGHBHSDJH";
+            string name = "Johnny English";
+            string txt2 = " is lengendary";
+
+            // string Concatenation
+            string cnc = name + txt2;
+
+            Console.WriteLine("The txt Lenghth is: " + txt.Length);
+            Console.WriteLine("To lowercase: " + txt.ToLower());
+            Console.WriteLine("To Uppercase: " + name.ToUpper());
+        }
+    }
+}
