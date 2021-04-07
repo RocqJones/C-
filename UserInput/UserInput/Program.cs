5c9ce0427ca1637eac5e5d76334f004df21181d1
RocqJones <jonesmbindyo@gmail.com>
2021-04-07 11:49:57 +0300

    gettig user input

--- UserInput/UserInput/Program.cs
+++ UserInput/UserInput/Program.cs
@@ -12,8 +12,9 @@ namespace UserInput
             Console.WriteLine("Enter Second Name? ");
             string sName = Console.ReadLine();
 
+            // since you cannot implicitly convert type 'string' to 'int' convert
             Console.WriteLine("Your age?");
-            string uAge = Console.ReadLine();
+            int uAge = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Your full name is: " + fName + " " + sName + " and you are " + uAge + " years old");
         }
