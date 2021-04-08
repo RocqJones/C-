b85f845bdb613ca8e43d42e1e6f2a535cb217309
RocqJones <jonesmbindyo@gmail.com>
2021-04-08 10:40:44 +0300

    arrays

---
+++ ArraysL/ArraysL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArraysL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // def arr
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
