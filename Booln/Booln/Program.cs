598ab9b0459f39c654de86b4e06fc1b9032e9648
RocqJones <jonesmbindyo@gmail.com>
2021-04-08 06:31:33 +0300

    boolean

---
+++ Booln/Booln/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Booln
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 23;
+
+            Console.WriteLine(x < y);
+            Console.WriteLine(x > y);
+        }
+    }
+}
