5c06ae743b3c35a7e904d2bf1d7dc1e03f11ade4
RocqJones <jonesmbindyo@gmail.com>
2021-04-08 06:39:51 +0300

    if else statement

---
+++ IfConditionalBlock/IfConditionalBlock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IfConditionalBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 15;
+
+            if (time < 10)
+            {
+                Console.WriteLine("Good morning.");
+            } 
+            else if (time < 20)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening");
+            }
+        }
+    }
+}
