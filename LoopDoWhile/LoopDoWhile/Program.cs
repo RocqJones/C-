206f7acb22ef6fabb0eee2e1d9feedfe204ad1e1
RocqJones <jonesmbindyo@gmail.com>
2021-04-08 07:05:34 +0300

    while and do-while loop

---
+++ LoopDoWhile/LoopDoWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LoopDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 1;
+
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j <= 10);
+        }
+    }
+}
