6069e69a84e33838b268790939f9e30bf9f82c9b
RocqJones <jonesmbindyo@gmail.com>
2021-04-08 10:22:26 +0300

    for loop

---
+++ LoopFor/LoopFor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // even numbers
+            for (int j = 2; j <= 10; j = j + 2)
+            {
+                Console.WriteLine("Even num: " + j);
+            }
+
+            // odd numbers
+            for (int k = 1; k <= 10; k = k + 2)
+            {
+                Console.WriteLine("Odd num: " + k);
+            }
+        }
+    }
+}
