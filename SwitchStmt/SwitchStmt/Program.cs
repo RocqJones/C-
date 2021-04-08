9be50898614479f6911d7aed0b801575e05d82d4
RocqJones <jonesmbindyo@gmail.com>
2021-04-08 06:46:36 +0300

    switch stmt

---
+++ SwitchStmt/SwitchStmt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwitchStmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 7;
+
+            switch (day)
+            {
+                case 4:
+                    Console.WriteLine("It's Thurday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("It's Friday");
+                    break;
+
+                default:
+                    Console.WriteLine("Looking forward to the weekend");
+                    break;
+            }
+        }
+    }
+}
