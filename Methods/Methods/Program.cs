6da2a29c319a0e9a9fc8a197bd7bd8dd987ef787
RocqJones <jonesmbindyo@gmail.com>
2021-04-16 10:44:41 +0300

    methods plus prams

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void myMethod()
+        {
+            Console.WriteLine("Run me");
+        }
+
+        // Param arguments and default params
+        static void myMethodTwo(string fname, string sname = "Telles")
+        {
+            Console.WriteLine(fname + " " + sname);
+        }
+
+        static int doCalc(int x, int y)
+        {
+            double z = 122.67;
+
+            return (int)(double) ((x * z) / y);
+        }
+
+        static void Main(string[] args)
+        {
+            myMethod();
+            myMethodTwo("John"); // sname picks from default param
+            myMethodTwo("Maguire", "Loise");
+            Console.WriteLine(doCalc(23, 45));
+        }
+    }
+}
