004ae4a13a591af076367c9804168450abbaf7d0
RocqJones <jonesmbindyo@gmail.com>
2021-04-19 10:35:49 +0300

    Class members: Fields and methods in a class

---
+++ OOP directory/ClassMembers/ClassMembers/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMembers
+{
+    // Fields - They are variables inside a class
+    class MyClass
+    {
+        // class members - Fields and methods inside classes
+        public string model;            // field
+        public string color = "blue";  // field
+        public int maxSpeed = 320;     // field
+        public int year;               // field
+
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going fast as it can!");
+        }
+    }
+}
--- OOP directory/ClassMembers/ClassMembers/Program.cs
+++ OOP directory/ClassMembers/ClassMembers/Program.cs
@@ -6,7 +6,14 @@ namespace ClassMembers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyClass newObj = new MyClass();
+            newObj.model = "Ford Mustang";
+            newObj.year = 2018;
+
+            Console.WriteLine(newObj.model + " was manufactured in the year " + newObj.year +
+                " and the " + newObj.color + " car has top speed of " + newObj.maxSpeed + "km/h.");
+
+            newObj.fullThrottle();
         }
     }
 }
