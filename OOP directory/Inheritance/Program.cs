f5cce3b24dc6911a18510ff41dc87a75c3eef631
RocqJones <jonesmbindyo@gmail.com>
2021-04-19 13:58:24 +0300

    Inheritance

---
+++ OOP directory/Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inheritance
+{
+    // base (parent) class
+    class Vehicle
+    {
+        public string brand = "BMW";    // vehicle field
+        public void honk()              // vehicle method
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    // derived (child) class
+    class Car : Vehicle
+    {
+        public string modelName = "M5 Competition"; // car field
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.honk();
+            Console.WriteLine(myCar.brand + " " +  myCar.modelName);
+        }
+    }
+}
