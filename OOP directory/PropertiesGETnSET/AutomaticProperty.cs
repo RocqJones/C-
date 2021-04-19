82664ad5318cda02a480c3c66719a02acc4aa809
RocqJones <jonesmbindyo@gmail.com>
2021-04-19 12:52:50 +0300

    Properties and Encapsulation

---
+++ OOP directory/PropertiesGETnSET/AutomaticProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PropertiesAndEncapsulation
+{
+    // Automatic Properties (Short Hand)
+    class AutomaticProperty
+    {
+        // No need to define the field for the property. You only have to write get; and set; inside the property.
+        public string Name
+        {
+            get; set;
+        }
+    }
+
+    class ProgramMain
+    {
+        static void main(string[] args)
+        {
+            AutomaticProperty aObj = new AutomaticProperty();
+            aObj.Name = "Luise Lewadoski";
+            Console.WriteLine(aObj.Name);
+        }
+    }
+}
---
+++ OOP directory/PropertiesGETnSET/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PropertiesGETnSET
+{
+    class Person
+    {
+        // field
+        private string name;
+
+        // property
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "John Lenon";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
