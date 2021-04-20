8ccaf6c64085b8f85d46a441c56460e3dab6cd9a
RocqJones <jonesmbindyo@gmail.com>
2021-04-20 16:13:39 +0300

    Abstraction

---
+++ OOP directory/Abstraction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Abstraction
+{
+    // Abstract class
+    abstract class Animal
+    {
+        // abstract method
+        public abstract void animalSound();
+        // Regular method
+        public void sleep()     
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    // derived class (inherit from animal)
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Pig object
+            Pig mypig = new Pig();
+            mypig.animalSound();
+            mypig.sleep();
+        }
+    }
+}
