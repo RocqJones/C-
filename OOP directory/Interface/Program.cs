fad8b6b766aba77ac6680c67c0c3beb71af53554
RocqJones <jonesmbindyo@gmail.com>
2021-04-21 22:16:24 +0300

    Interface

---
+++ OOP directory/Interface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Interface
+{
+    // interface
+    interface IAnimal
+    {
+        void animalSound(); // interface method (does not have a body)
+    }
+
+    // add another intercases
+    interface ISecondInterface
+    {
+        void myOtherMethod();
+    }
+
+    // Pig "implements" the IAnimal interface
+    class Pig : IAnimal, ISecondInterface
+    {
+        public void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+
+        public void myOtherMethod()
+        {
+            Console.WriteLine("Another interface method implemented");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create object
+            Pig pig = new Pig();
+            pig.animalSound();
+            pig.myOtherMethod();
+        }
+    }
+}
