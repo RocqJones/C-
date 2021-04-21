711c45b9244c68f8139f20816fb5360fedc49244
RocqJones <jonesmbindyo@gmail.com>
2021-04-21 23:05:43 +0300

    Enums with Switch statement

---
+++ OOP directory/EnumsWithSwitch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EnumsWithSwitch
+{
+    enum Levels
+    {
+        Low,
+        Medium,
+        High
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Levels levels = Levels.High;
+
+            switch (levels)
+            {
+                case Levels.Low:
+                    Console.WriteLine("Low levels here");
+                    break;
+
+                case Levels.Medium:
+                    Console.WriteLine("Medium levels here");
+                    break;
+
+                case Levels.High:
+                    Console.WriteLine("High levels here");
+                    break;
+            }
+        }
+    }
+}
