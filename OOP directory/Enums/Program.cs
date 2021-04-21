6c84b72b76c7b23b48caea35e1b10acf01c4dc4b
RocqJones <jonesmbindyo@gmail.com>
2021-04-21 22:57:18 +0300

    Enums inside a class

---
+++ OOP directory/Enums/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        enum Level
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        static void Main(string[] args)
+        {
+            Level myVar = Level.Medium;
+            Console.WriteLine(myVar);
+        }
+    }
+}
