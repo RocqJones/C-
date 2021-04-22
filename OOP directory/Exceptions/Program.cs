487b2d9e0b73c6f913db829e4c0658d90b89f17f
RocqJones <jonesmbindyo@gmail.com>
2021-04-22 09:08:22 +0300

    try...catch and finally

--- OOP directory/Exceptions/Program.cs
+++ OOP directory/Exceptions/Program.cs
@@ -6,7 +6,22 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int[] myNums = { 1, 2, 3 };
+                Console.WriteLine(myNums[10]);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            // finally statement lets you execute code, after try...catch, regardless of the result
+            finally
+            {
+                Console.WriteLine("The 'try catch' block finished!");
+            }
         }
     }
 }
