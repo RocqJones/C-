cf7d7b606877c92accc06c2102633e6dcaaf2f91
RocqJones <jonesmbindyo@gmail.com>
2021-04-22 08:44:53 +0300

    File handling

---
+++ OOP directory/Files/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO; // include input/output namespace
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeTxt = "Hello! this is my first file handling files with C#";
+
+            // write to file
+            File.WriteAllText("example.txt", writeTxt);
+
+            // read from file and display output
+            string readTxt = File.ReadAllText("example.txt");
+            Console.WriteLine(readTxt);
+        }
+    }
+}
